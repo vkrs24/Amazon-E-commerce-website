e30fa4ebd7e5a55269061a066d247dbec83104d1
Vasanth Kumar <mybusiness.vkrs@gmail.com>
2025-01-06 08:45:11 +0530

    Make the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -73,6 +73,14 @@ document.querySelectorAll(".js-add-to-cart").forEach((button) => {
     } else {
       cart.push({ productId: productId, quantity: 1 });
     }
+
+    let cartQuantity = 0;
+    cart.forEach((item) => {
+      cartQuantity += item.quantity;
+    });
+
+    document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
+
     console.log(cart);
   });
 });
