a91c0fca47ad15ac7afb2b65ec28b645e7a56651
Vasanth Kumar <mybusiness.vkrs@gmail.com>
2025-01-11 15:38:53 +0530

    delete cart when clicked

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -14,7 +14,9 @@ cart.forEach((cartItem) => {
     }
   });
   cartSummaryHTML += `
-    <div class="cart-item-container">
+    <div class="cart-item-container js-cart-item-container-${
+      matchingProduct.id
+    }">
           <div class="delivery-date">
             Delivery date: Tuesday, June 21
           </div>
@@ -100,6 +102,9 @@ document.querySelectorAll(".js-delete-link").forEach((link) => {
   link.addEventListener("click", () => {
     const productID = link.dataset.productId;
     removecart(productID);
-    console.log(cart);
+    const container = document.querySelector(
+      `.js-cart-item-container-${productID}`
+    );
+    container.remove();
   });
 });
